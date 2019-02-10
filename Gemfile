a5610167703352c0e3970dfb87b97de451b62477
Lucas Molander <lmmoland@ncsu.edu>
2019-02-09 22:15:06 -0800

    Finally able to test site locally.....

--- Gemfile
+++ Gemfile
@@ -1,3 +1,5 @@
 # IDK why this is here, but this page told me to do it: https://help.github.com/articles/adding-a-jekyll-theme-to-your-github-pages-site/
 
-gem "cayman"
+source 'https://rubygems.org'
+gem 'github-pages', group: :jekyll_plugins
+
---
+++ serve.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#
+# Use this shell script to run Jekyll locally.
+#
+# If you're unfamiliar with Jekyll, you have to:
+#   1) Install Ruby (make sure to press 3!)
+#   2) Install the other stuff - see: https://help.github.com/articles/setting-up-your-github-pages-site-locally-with-jekyll/
+#
+
+bundle exec jekyll serve
+
